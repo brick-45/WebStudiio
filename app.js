8f23c6101346744f6333794c5dd8b7a9c37f4bf8
brick-45 <polishtshuksoffia@gmail.com>
2026-09-06 11:30:20 +0200

    braaaaaaaaaaaaaaa

---
+++ app.js
@@ -0,0 +1,21 @@
+const kokos = document.querySelector(".command");
+
+console.log(kokos.parentNode);
+
+console.log(kokos.childNodes);
+
+console.log(kokos.children);
+
+
+console.log(kokos.firstChild);
+console.log(kokos.lastChild);
+
+console.log(kokos.firstElementChild);
+console.log(kokos.lastElementChild);
+
+console.log(kokos.previousSibling);
+console.log(kokos.nextSibling);
+
+console.log(kokos.previousElementSibling);
+console.log(kokos.nextElementSibling);
+
